6bb9799bc723911a73ec6920c94881268cf5668a
Georgii Ishchenko <ishchenko.gv@gmail.com>
2024-12-25 17:49:10 +0000

    cleanup

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -1,9 +1,11 @@
 const TelegramBot = require("node-telegram-bot-api");
+
 const { checkSpelling } = require("../spell-checker");
 const { checkFreeAttempts } = require("../subscription");
 
 /**
- * @typedef {import('node-telegram-bot-api').TelegramBot}
+ * @typedef {import('node-telegram-bot-api').TelegramBot} TelegramBot
+ * @typedef {import('node-telegram-bot-api').CallbackQuery} CallbackQuery
  */
 
 /**
@@ -36,45 +38,53 @@ function runTelegramBot() {
     console.log(`[polling_error] ${error.code}: ${error.message}`);
   });
 
-  _bot.onText(/\/test_offer/, (msg) => {
+  _bot.onText(/\/test_offer/, offerSubscription);
+
+  _bot.on("message", handleMessage);
+  _bot.on("callback_query", handleCallbackQuery);
+}
+
+/**
+ * @param {string} msg
+ * @returns {void}
+ */
+async function handleMessage(msg) {
+  if (msg.text.startsWith("/")) {
+    return;
+  }
+
+  const userId = msg.from.id;
+  const chatId = msg.chat.id;
+
+  const freeAttemptsRemained = await checkFreeAttempts(userId);
+
+  if (!freeAttemptsRemained) {
     offerSubscription(msg);
-  });
+    return;
+  }
 
-  _bot.on("message", async (msg) => {
-    if (msg.text.startsWith("/")) {
-      return;
-    }
+  const answer = await checkSpelling(msg.text);
 
-    const userId = msg.from.id;
-    const chatId = msg.chat.id;
+  _bot.sendMessage(chatId, answer);
+}
 
-    const freeAttemptsRemained = await checkFreeAttempts(userId);
+/**
+ * @param {CallbackQuery} callbackQuery
+ */
+function handleCallbackQuery(callbackQuery) {
+  const chatId = callbackQuery.message.chat.id;
+  const command = callbackQuery.data;
 
-    if (!freeAttemptsRemained) {
-      offerSubscription(msg);
-      return;
-    }
-
-    const answer = await checkSpelling(msg.text);
-
-    _bot.sendMessage(chatId, answer);
-  });
-
-  _bot.on("callback_query", (callbackQuery) => {
-    const chatId = callbackQuery.message.chat.id;
-    const command = callbackQuery.data;
-
-    switch (command) {
-      case commands.SUBSCRIBE_MONTH:
-        _bot.sendInvoice(...Object.values(getMonthSubscriptionInvoice(chatId)));
-        break;
-      case commands.SUBSCRIBE_YEAR:
-        _bot.sendInvoice(...Object.values(getYearSubscriptionInvoice(chatId)));
-        break;
-      default:
-        console.error("Unknown callback query:", command);
-    }
-  });
+  switch (command) {
+    case commands.SUBSCRIBE_MONTH:
+      _bot.sendInvoice(...Object.values(getMonthSubscriptionInvoice(chatId)));
+      break;
+    case commands.SUBSCRIBE_YEAR:
+      _bot.sendInvoice(...Object.values(getYearSubscriptionInvoice(chatId)));
+      break;
+    default:
+      console.error("Unknown callback query:", command);
+  }
 }
 
 /**
