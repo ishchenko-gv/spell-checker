4f804efdba6395d82293551557f30f81fd3b2d6b
Georgii Ishchenko <ishchenko.gv@gmail.com>
2024-12-25 12:06:13 +0000

    add seeds

---
+++ seeds/plans.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex("plans").del();
+  await knex("plans").insert([
+    {
+      id: 1,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+      duration_months: 1,
+      price_tg_stars: 10,
+    },
+    {
+      id: 2,
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now(),
+      duration_months: 10,
+      price_tg_stars: 100,
+    },
+  ]);
+};
--- test/setup.js
+++ test/setup.js
@@ -11,7 +11,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-  // await db.seed.run();
+  await db().seed.run();
 });
 
 afterAll(async () => {
